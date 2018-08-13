82072b0c783218d90ce6c73d878d73073e035741
piotryfon <pit.pit@wp.pl>
2018-08-13 15:54:15 +0200

    function makeArray()

--- main.js
+++ main.js
@@ -2,9 +2,21 @@
     var table = document.querySelector("#myTable"),
     ths = table.querySelectorAll("thead th"),
     trs = table.querySelectorAll("tbody tr");
-   
-    function sortBy(){
-        console.log("sortuję!!!")
+    
+    function makeArray(nodeList){
+        let arr = [];
+        for(let i = 0; i < nodeList.length; i++){
+            arr.push(nodeList[i]);
+        }
+        return arr;
+    }
+
+    function sortBy(e){
+       
+        let target = e.target
+        thsArr = makeArray(ths)
+        console.log(target)
+        console.log(thsArr)
     }
     for(var i = 0; i < ths.length; i++){
         ths[i].onclick = sortBy;
