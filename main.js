c9ec474849331dc748f857c1a8f9fa48a2dd7c6e
piotryfon <pit.pit@wp.pl>
2018-08-13 16:59:55 +0200

    sort td

--- main.js
+++ main.js
@@ -15,10 +15,30 @@
        
         let target = e.target,
         thsArr = makeArray(ths),
+        trsArr = makeArray(trs),
         index = thsArr.indexOf(target)
+
+        trsArr.sort(function(a, b){
+            let tdA = a.children[index].textContent,
+                tdB = b.children[index].textContent;
+
+            if(tdA < tdB){
+                return 1;
+            }else if(tdA > tdB){
+                return -1;
+            }else {
+                return 0
+            };
+
+            console.log(tdB)
+
+        });
+        /*
         console.log(target)
         console.log(thsArr)
         console.log(index)
+         */
+        console.log(trsArr)
     }
     for(var i = 0; i < ths.length; i++){
         ths[i].onclick = sortBy;
